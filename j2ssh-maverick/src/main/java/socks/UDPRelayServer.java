5ddc8a21111479c84b39abefe80e35d91b90320b
Lee David Painter <lee@ludup.com>
2015-08-15 16:31:45 +0100

    Added extra source header for socks project.

--- j2ssh-maverick/src/main/java/socks/UDPRelayServer.java
+++ j2ssh-maverick/src/main/java/socks/UDPRelayServer.java
@@ -1,3 +1,27 @@
+/**
+ * Copyright 2003-2014 SSHTOOLS Limited. All Rights Reserved.
+ *
+ * For product documentation visit https://www.sshtools.com/
+ *
+ * This file is part of J2SSH Maverick.
+ *
+ * J2SSH Maverick is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * J2SSH Maverick is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with J2SSH Maverick.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/**
+ * This file is originally from the http://sourceforge.net/projects/jsocks/
+ * released under the LGPL.
+ */
 package socks;
 import socks.server.*;
 import java.net.*;
