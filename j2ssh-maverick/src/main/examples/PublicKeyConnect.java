b3f494ef2fda4b8c71449d2579949ad353edfbc0
Lee David Painter <lee@ludup.com>
2016-01-27 10:50:12 +0000

    Update PublicKeyConnect.java

--- j2ssh-maverick/src/main/examples/PublicKeyConnect.java
+++ j2ssh-maverick/src/main/examples/PublicKeyConnect.java
@@ -96,7 +96,7 @@ public class PublicKeyConnect {
 			 * Connect to the host
 			 */
 			SshClient ssh = con.connect(new SocketTransport(hostname, port),
-					username);
+					username, true);
 
 			/**
 			 * Authenticate the user using password authentication
