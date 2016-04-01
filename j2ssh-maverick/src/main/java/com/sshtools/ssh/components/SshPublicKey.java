aa496ba410f4037f212c42e974badf5807644d9f
Lee David Painter <lee@ludup.com>
2016-04-01 13:08:11 +0100

    Updated headers to LGPL source header.

--- j2ssh-maverick/src/main/java/com/sshtools/ssh/components/SshPublicKey.java
+++ j2ssh-maverick/src/main/java/com/sshtools/ssh/components/SshPublicKey.java
@@ -1,12 +1,12 @@
 /**
- * Copyright 2003-2014 SSHTOOLS Limited. All Rights Reserved.
+ * Copyright 2003-2016 SSHTOOLS Limited. All Rights Reserved.
  *
  * For product documentation visit https://www.sshtools.com/
  *
  * This file is part of J2SSH Maverick.
  *
  * J2SSH Maverick is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
@@ -18,7 +18,6 @@
  * You should have received a copy of the GNU General Public License
  * along with J2SSH Maverick.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package com.sshtools.ssh.components;
 
 import com.sshtools.ssh.SshException;
